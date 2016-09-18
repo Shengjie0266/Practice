47fc3bd8dd37c18e9ae4179e8e813f64af557cda
Shengjie0266 <105021040@live.asia.edu.tw>
2016-09-18 19:09:04 +0800

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,14 @@
+package hw;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 47, b = 34;
+		int c = a + b;
+		System.out.println(c);
+		System.out.println(a * b);
+		System.out.println((float)a / b);
+	}
+
+}
